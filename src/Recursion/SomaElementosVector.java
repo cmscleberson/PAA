87f8824a8652f1b55c4c4bcc1076a25ae52ad0d0
Cleberson Marcelo dos Santos <33093380+cmscleberson@users.noreply.github.com>
2019-12-23 11:49:40 -0300

    Exercicios Recursividade

--- src/Recursion/SomaElementosVector.java
+++ src/Recursion/SomaElementosVector.java
@@ -1,4 +1,19 @@
 package Recursion;
 
 public class SomaElementosVector {
+
+    static int soma(int v[], int n) {
+        if (n <= 0) {
+            return v[n];
+        } else {
+            return v[n] + soma(v, n - 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int v[] = {10, 20, 30, 40};
+        System.out.println(soma(v, v.length - 1));
+    }
 }
