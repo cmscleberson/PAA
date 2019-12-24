9bbd6d44cd605398b46b254c046918ebd6ec91d5
Cleberson Marcelo dos Santos <33093380+cmscleberson@users.noreply.github.com>
2019-12-24 11:20:53 -0300

    Exercicios Recursividade

---
+++ src/Recursion/OcorrenciasDeKemC.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class OcorrenciasDeKemC {
+    static int ocorrencias(int k, int n, int i) {
+        int div = n % 10;
+        if (div == k) {
+            i++;
+        }
+
+        if (n % k == n) {
+            return i;
+        }
+
+        return ocorrencias(k, n / 10, i);
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ocorrencias(4, 24342, 0));
+    }
+}
